7d57135a9acd3833c14c0a77d15443e6aad32f38
ben awad <benawad97@gmail.com>
2016-11-05 18:19:37 -0500

    Read aws bucket and return json

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ const spawn = require('child_process').spawn;
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
+const AWS = require('aws-sdk'); 
+
+AWS.config.update({
+    accessKeyId: "AKIAJURJT6TKRTQZABTA",
+    secretAccessKey: "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"
+});
+
+const s3 = new AWS.S3(); 
+
+function readFile(project, callback) {
+  const params = {Bucket: 'sasp', Key: project + '/main.txt'};
+  s3.getObject(params, callback);
+}
 
 app.use(bodyParser.json());
 
@@ -15,11 +28,13 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.post('/query', (req, res) => {
+app.post('/:project/query', (req, res) => {
   const query = req.body.query;
 
-  fs.readFile(answerSet, 'utf8', (err, data) => {
+  readFile(req.params.project, (err, data) => {
     if (err) throw err;
+
+    data = data.Body.toString('ascii');
 
     const lines = data.split(os.EOL);
 
@@ -31,12 +46,16 @@ app.post('/query', (req, res) => {
         const sasp = spawn('sasp', ['-a', querySet]);
 
         sasp.stdout.on('data', (data) => {
-          res.send(`${data}`);
+          const strData = `${data}`;
+          const json = { "output": strData }
+          res.send(JSON.stringify(json));
           console.log(`stdout: ${data}`);
         });
 
         sasp.stderr.on('data', (data) => {
-          res.send(`${data}`);
+          const strData = `${data}`;
+          const json = { "output": strData }
+          res.send(JSON.stringify(json));
           console.log(`stderr: ${data}`);
         });
 
